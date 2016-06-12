98835984c34dac6a3ef3baac1d5e268210145833
Alex <alex@alex-VirtualBox.(none)>
2016-06-12 22:09:59 +0300

    GET logout bug fixed

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,11 @@ Rails.application.routes.draw do
   root 'users#index'
 
   resources :users, except: [:destroy]
-  resources :sessions, only: [:new, :create, :destroy]
+  resource :session, only: [:new, :create, :destroy]
   resources :questions, except: [:show, :new, :index]
 
-  get 'sign_up' => 'users#new'
-  get 'log_out' => 'sessions#destroy'
-  get 'log_in' => 'sessions#new'
+  # get 'sign_up' => 'users#new'
+  # get 'log_out' => 'sessions#destroy'
+  # get 'log_in' => 'sessions#new'
 
 end
