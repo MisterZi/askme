a3aaca52211f6a0e678d8ccc5ab3c2ec7707115f
Alex <alex@alex-VirtualBox.(none)>
2016-06-12 12:24:38 +0300

    questions counter

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,16 @@
+# Контроллер, управляющий сессиями
+# 1. Показывает страницу логина
+# 2. Создает новую сессию (логин)
+# 3. Позволяет разлогиниваться (удаляет сессию)
+#
 class SessionsController < ApplicationController
+
+  # пустой экшен, только показывает свой шаблон
   def new
   end
 
+  # создает в объекте session новый факт залогиненности пользователя
+  # если он правильно сообщил мэйл/пароль
   def create
     @user = User.authenticate(params[:email], params[:password])
 
@@ -9,13 +18,15 @@ class SessionsController < ApplicationController
       session[:user_id] = @user.id
       redirect_to root_url, notice: 'вы успешно залогинились'
     else
-      flash.new.alert = 'Неправильный мэйл или пароль'
+      flash.now.alert = 'Неправильный мэйл или пароль'
       render :new
     end
   end
 
+  # удаляет сессию залогиненного юзера
   def destroy
     session[:user_id] = nil
     redirect_to root_url, notice: 'Вы разлогинились! Приходите еще!'
   end
+
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -17,4 +17,37 @@ module ApplicationHelper
   def fa_icon(icon_class)
     content_tag 'span', '', class: "fa fa-#{icon_class}"
   end
+
+
+  # Хелпер, склоняющий слова
+  def sklonenie(number, krokodil, krokodila, krokodilov, with_number = false)
+    # проверим входные данные на правильность
+    if (number == nil || !number.is_a?(Numeric))
+      number = 0 # если первый параметр пустой или не число, то продолжаем как будто он нулевой
+    end
+
+    prefix = ""
+    prefix = "#{number} " if with_number
+
+    ostatok = number % 10 # склонение определяется последней цифрой в числе
+
+    ostatok100 = number % 100
+    if (ostatok100 >= 11 && ostatok100 <= 14)
+      return "#{prefix}#{krokodilov}"
+    end
+
+    if (ostatok == 1) # для 1 - именительный падеж (Кто? Что?)
+      return "#{prefix}#{krokodil}"
+    end
+
+    if (ostatok >= 2 && ostatok <= 4) # для 2-4 - родительный падеж (Кого? Чего?)
+      return "#{prefix}#{krokodila}"
+    end
+
+    # 5-9 или ноль – родительный падеж и множественное число
+    if (ostatok >= 5 && ostatok <= 9 || ostatok == 0)
+      return "#{prefix}#{krokodilov}"
+    end
+  end
+
 end
